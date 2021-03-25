bd24240379442d9204f350e3c5b395057e3f35d7
salvador <josesalvadorpp@hotmail.com>
2021-03-25 03:43:24 -0400

    setting server

--- client/src/components/toastr.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-class Toastr extends Component {
-	render() {
-		return <ToastContainer />;
-	}
-}
-export default Toastr;
