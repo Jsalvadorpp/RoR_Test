31b427c7dddbe73fcb8d9ed1f96abd85988da4ba
salvador <josesalvadorpp@hotmail.com>
2021-03-25 03:57:05 -0400

    update base_url for api

--- client/src/api.js
+++ client/src/api.js
@@ -6,4 +6,7 @@ export default {
 };
 
 //Local server
-export const base_url = 'http://127.0.0.1:3000';
+//export const base_url = 'http://127.0.0.1:3000';
+
+//Heroku Server
+export const base_url = 'https://obscure-refuge-85543.herokuapp.com';
